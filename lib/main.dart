ef7fef1043046230e52e7cfb8bb1aecbb6a05a25
alex-gamero <matias10gamero@gmail.com>
2025-09-19 22:55:21 -0400

    Lab 3 (edge) completed

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
@@ -30,85 +31,191 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var myFontSize = 30.0;
-  var fileToShow = "images/question-mark.png";
-  late TextEditingController _controller; //late means promise to initialize it later
-  var _login = TextEditingController();
-
-
-  //you're visible
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController(); //doing your promise to initialize
-  }
-
-  //you are being removed
-  @override
-  void dispose() {
-    super.dispose();
-    //free memory:
-    _controller.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
 
+            Text("BROWSE CATEGORIES", style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold)),
+            Text("\n", style: TextStyle(fontSize: 8.0, fontWeight: FontWeight.bold)),
 
-            Padding(child:
-            TextField(controller: _login, decoration:
-            InputDecoration(
-                border: OutlineInputBorder(),
-                hintText:"Login",
-                labelText: "Login"
+            Text("Not sure about exactly which recipe you're looking for? Do a search, or dive into our most popular categories", style: TextStyle(fontSize: 15.0)),
+            Text("\n", style: TextStyle(fontSize: 11.0, fontWeight: FontWeight.bold)),
 
-            ) ),
-              padding: EdgeInsets.fromLTRB(50, 0, 50, 0)
+            Text("BY MEAT", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
+            Text("\n", style: TextStyle(fontSize: 7.0, fontWeight: FontWeight.bold)),
+
+            Row(
+              crossAxisAlignment:CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+              children: <Widget> [
+
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                        backgroundImage: AssetImage('images/beef.jpg'),
+                        radius:80
+                    ),
+                    Text("BEEF", style: TextStyle(fontSize: 25.0, color: Colors.white))
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                        backgroundImage: AssetImage('images/chicken.jpg'),
+                        radius:80
+                    ),
+                    Text("CHICKEN", style: TextStyle(fontSize: 25.0, color: Colors.white ))
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                        backgroundImage: AssetImage('images/pork.jpg'),
+                        radius:80
+                    ),
+                    Text("PORK", style: TextStyle(fontSize: 25.0, color: Colors.white ))
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                        backgroundImage: AssetImage('images/seafood.jpg'),
+                        radius:80
+                    ),
+                    Text("SEAFOOD", style: TextStyle(fontSize: 25.0, color: Colors.white ))
+                  ],
+                )
+
+              ]
+
             ),
 
-            Padding(child:
-            TextField(controller: _controller, decoration:
-            InputDecoration(
-                border: OutlineInputBorder(),
-                hintText:"Password",
-                labelText: "Password"
+            Text("\n", style: TextStyle(fontSize: 11.0, fontWeight: FontWeight.bold)),
+            Text("BY COURSE", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
+            Text("\n", style: TextStyle(fontSize: 7.0, fontWeight: FontWeight.bold)),
+
+            Row(
+                crossAxisAlignment:CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+                children: <Widget> [
+
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      CircleAvatar(
+                          backgroundImage: AssetImage('images/main.jpg'),
+                          radius:80
+                      ),
+                      Text("\n", style: TextStyle(fontSize: 2.0, fontWeight: FontWeight.bold)),
+                      Text("Main Dishes", style: TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      CircleAvatar(
+                          backgroundImage: AssetImage('images/salad.jpg'),
+                          radius:80
+                      ),
+                      Text("\n", style: TextStyle(fontSize: 2.0, fontWeight: FontWeight.bold)),
+                      Text("Salad Recipes", style: TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      CircleAvatar(
+                          backgroundImage: AssetImage('images/side.jpg'),
+                          radius:80
+                      ),
+                      Text("\n", style: TextStyle(fontSize: 2.0, fontWeight: FontWeight.bold)),
+                      Text("Side Dishes", style: TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      CircleAvatar(
+                          backgroundImage: AssetImage('images/pot.jpg'),
+                          radius:80
+                      ),
+                      Text("\n", style: TextStyle(fontSize: 2.0, fontWeight: FontWeight.bold)),
+                      Text("Crockpot", style: TextStyle(fontSize: 20.0))
+                    ],
+                  )
+
+                ]
 
             ),
-                obscureText: true
-            ),
-                padding: EdgeInsets.fromLTRB(50, 0, 50, 0)
-            ),
 
-            ElevatedButton(onPressed: () { //paste the text written
-              setState((){
+            Text("\n", style: TextStyle(fontSize: 11.0, fontWeight: FontWeight.bold)),
+            Text("BY DESSERT", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
+            Text("\n", style: TextStyle(fontSize: 7.0, fontWeight: FontWeight.bold)),
 
-                var txt = _controller.value.text;
-                setState(() {
-                  if(txt == "QWERTY123"){
-                    fileToShow = "images/idea.png";
-                  }
-                  else {
-                    fileToShow = "images/stop.png";
-                  }
-                });
+            Row(
+                crossAxisAlignment:CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
 
-              });
+                children: <Widget> [
 
-            } //<-- Lambda, or anonymous function
-                , child: Text("Login")),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      CircleAvatar(
+                          backgroundImage: AssetImage('images/ice.jpg'),
+                          radius:80
+                      ),
+                      Text("\n", style: TextStyle(fontSize: 2.0, fontWeight: FontWeight.bold)),
+                      Text("Ice Cream", style: TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      CircleAvatar(
+                          backgroundImage: AssetImage('images/brownies.jpg'),
+                          radius:80
+                      ),
+                      Text("\n", style: TextStyle(fontSize: 2.0, fontWeight: FontWeight.bold)),
+                      Text("Brownies", style: TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      CircleAvatar(
+                          backgroundImage: AssetImage('images/pie.jpg'),
+                          radius:80
+                      ),
+                      Text("\n", style: TextStyle(fontSize: 2.0, fontWeight: FontWeight.bold)),
+                      Text("Pies", style: TextStyle(fontSize: 20.0))
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      CircleAvatar(
+                          backgroundImage: AssetImage('images/cookies.jpg'),
+                          radius:80
+                      ),
+                      Text("\n", style: TextStyle(fontSize: 2.0, fontWeight: FontWeight.bold)),
+                      Text("Cookies", style: TextStyle(fontSize: 20.0))
+                    ],
+                  )
 
-            Semantics(child: Image.asset(fileToShow, height:100, width:100))
+                ]
 
-
+            )
 
           ],
         ),
